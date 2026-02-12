c11554f0703be26770b1413460d22309a5184aa2
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-12 10:16:27 +0800

    feat: colorscheme

--- lua/plugin/colorscheme.lua
+++ lua/plugin/colorscheme.lua
@@ -1,3 +1,14 @@
 -- INFO:
 -- See https://github.com/topics/neovim-colorscheme to find one you like and set it here
-return {}
+return {
+  'ellisonleao/gruvbox.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function(_, opts)
+    require('gruvbox').setup()
+    vim.o.background = 'dark'
+    vim.cmd.colorscheme('gruvbox')
+    vim.api.nvim_set_hl(0, 'FoldColumn', { fg = '#928374', bg = 'NONE', force = true })
+  end,
+}
