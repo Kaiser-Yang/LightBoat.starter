9dc25b8893eedd79c48072cf96cad6734855acaa
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-13 14:58:10 +0800

    chore: use ftplugin to bind keys

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,3 +1,10 @@
--- Delete the default keybindings for help buffers
-vim.keymap.del('n', '[[', { buffer = true })
-vim.keymap.del('n', ']]', { buffer = true })
+local h = require('lightboat.handler')
+local mapping = {
+  { { 'n', 'x', 'o' }, '[]', h.previous_section_end, { desc = 'Previous Block End', buffer = true } },
+  { { 'n', 'x', 'o' }, '][', h.next_section_end, { desc = 'Next Block End', buffer = true } },
+  { { 'n', 'x', 'o' }, '[[', h.previous_section_start, { desc = 'Previous Section', buffer = true } },
+  { { 'n', 'x', 'o' }, ']]', h.next_section_start, { desc = 'Next Section', buffer = true } },
+}
+for _, m in ipairs(mapping) do
+  vim.keymap.set(unpack(m))
+end
