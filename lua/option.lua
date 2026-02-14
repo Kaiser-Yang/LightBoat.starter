eedebae54c3e9907e64b322963af2a224bf731aa
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-14 14:37:56 +0800

    perf(big): big file handler

--- lua/option.lua
+++ lua/option.lua
@@ -1,11 +1,65 @@
 vim.g.big_file_limit = 1024 * 1024 -- 1 MB
 vim.g.big_file_average_every_line = 200
+local original = {}
+vim.g.big_file_callback = function(data)
+  if not original[data.buffer] then original[data.buffer] = {} end
+  if
+    not original[data.buffer].indentexpr
+    and vim.bo.indentexpr ~= ''
+    and vim.bo.indentexpr ~= "v:lua.require'nvim-treesitter'.indentexpr()"
+  then
+    original[data.buffer].indentexpr = vim.bo.indentexpr
+  end
+  if not original[data.buffer].buftype then original[data.buffer].buftype = vim.bo.buftype end
+  if data.new_status then
+    vim.b.conform_on_save = false
+    vim.b.treesitter_foldexpr_auto_set = false
+    vim.b.treesitter_indentexpr_auto_set = false
+    vim.b.treesitter_highlight_auto_start = false
+    vim.bo.indentexpr = original[data.buffer].indentexpr or ''
+    local treesitter_attached = nil
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      if vim.api.nvim_win_is_valid(win) and vim.api.nvim_win_get_buf(win) == data.buffer then
+        if vim.wo[win][0].foldexpr == "v:lua.require'nvim-treesitter'.foldexpr()" then treesitter_attached = true end
+        vim.wo[win][0].foldmethod = 'manual'
+        vim.wo[win][0].foldexpr = '0'
+        vim.api.nvim_set_current_win(win)
+      end
+    end
+    for _, client in pairs(vim.lsp.get_clients({ bufnr = data.buffer })) do
+      vim.lsp.buf_detach_client(data.buffer, client.id)
+    end
+    if vim.treesitter.highlighter.active[data.buffer] ~= nil then treesitter_attached = true end
+    -- This is used to make sure treesitter is stopped
+    if treesitter_attached then vim.cmd('e') end
+    if vim.bo.filetype == '' then
+      original[data.buffer].buftype = vim.bo.buftype
+      -- This is a tricky way to disable lsp attaching
+      -- Since lsp won't attach to buffers with buftype set to nonempty value
+      vim.bo.buftype = 'acwrite'
+      vim.api.nvim_create_autocmd('BufEnter', {
+        buffer = data.buffer,
+        once = true,
+        callback = function() vim.bo.buftype = original[data.buffer].buftype end,
+      })
+    end
+    require('nvim-treesitter.endwise').detach(data.buffer)
+  else
+    vim.b.conform_on_save = nil
+    vim.b.treesitter_foldexpr_auto_set = nil
+    vim.b.treesitter_indentexpr_auto_set = nil
+    vim.b.treesitter_highlight_auto_start = nil
+    -- Trigger the FileType autocommand to let lsp attach, indentexpr, endwise and foldexpr set up
+    if vim.bo.filetype ~= '' then vim.api.nvim_set_option_value('filetype', vim.bo.filetype, { buf = data.buffer }) end
+  end
+end
 vim.g.highlight_on_yank = true
 vim.g.highlight_on_yank_limit = 1024 -- 1 KB
 vim.g.highlight_on_yank_duration = 300
 vim.g.conform_on_save = true
 vim.g.conform_on_save_reguess_indent = true
 vim.g.conform_formatexpr_auto_set = true
+vim.g.treesitter_indentexpr_auto_set = true
 vim.g.treesitter_highlight_auto_start = true
 vim.g.treesitter_foldexpr_auto_set = true
 vim.g.nohlsearch_auto_run = true
