4f2bd9d8b34586815ec4d5268bb161a5ca92f63d
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-17 18:07:42 +0800

    feat(resolve)

--- lua/plugin/resolve.lua
+++ lua/plugin/resolve.lua
@@ -12,28 +12,33 @@ return {
     default_keymaps = false,
     on_conflict_detected = function(args)
       local h = require('lightboat.handler')
-      local previous_conflict = h.repmove_wrap('<plug>(resolve-prev)', '<plug>(resolve-next)', 1)
-      local next_conflict = h.repmove_wrap('<plug>(resolve-prev)', '<plug>(resolve-next)', 2)
+      local resolve = require('resolve')
+      local previous_conflict = h.repmove_wrap(resolve.prev_conflict, resolve.next_conflict, 1)
+      local next_conflict = h.repmove_wrap(resolve.prev_conflict, resolve.next_conflict, 2)
       local mapping = {
-        { 'n', '<leader>xc', '<plug>(resolve-ours)', { desc = 'Choose Current Conflict' } },
-        { 'n', '<leader>xi', '<plug>(resolve-theirs)', { desc = 'Choose Incoming Conflict' } },
-        { 'n', '<leader>xb', '<plug>(resolve-both)', { desc = 'Choose Both Conflict' } },
-        { 'n', '<leader>xB', '<plug>(resolve-both-reverse)', { desc = 'Choose Both Reverse Conflict' } },
-        { 'n', '<leader>xn', '<plug>(resolve-none)', { desc = 'Choose None Conflict' } },
-        { 'n', '<leader>xa', '<plug>(resolve-base)', { desc = 'Choose Ancestor Conflict' } },
-        { 'n', '<leader>xq', '<plug>(resolve-list)', { desc = 'Quickfix Conflict' } },
-        -- WARN: This only works for normal mode
-        { 'n', '[x', previous_conflict, { desc = 'Previous Git Conflict', expr = true } },
-        { 'n', ']x', next_conflict, { desc = 'Next Git Conflict', expr = true } },
+        { { 'n', 'x' }, '[x', previous_conflict, { desc = 'Previous Git Conflict' } },
+        { { 'n', 'x' }, ']x', next_conflict, { desc = 'Next Git Conflict' } },
+        { 'n', '<leader>xc', resolve.choose_ours, { desc = 'Choose Current Conflict' } },
+        { 'n', '<leader>xi', resolve.choose_theirs, { desc = 'Choose Incoming Conflict' } },
+        { 'n', '<leader>xb', resolve.choose_both, { desc = 'Choose Both Conflict' } },
+        { 'n', '<leader>xB', resolve.choose_both_reverse, { desc = 'Choose Both Reverse Conflict' } },
+        { 'n', '<leader>xn', resolve.choose_none, { desc = 'Choose None Conflict' } },
+        { 'n', '<leader>xa', resolve.choose_base, { desc = 'Choose Ancestor Conflict' } },
+        { 'n', '<leader>xq', resolve.list_conflicts, { desc = 'Quickfix Conflict' } },
       }
       if vim.fn.executable('delta') == 1 then
         mapping = vim.list_extend(mapping, {
-          { 'n', '<leader>xdi', '<plug>(resolve-diff-theirs)', { desc = 'Diff Incoming Conflict' } },
-          { 'n', '<leader>xdc', '<plug>(resolve-diff-ours)', { desc = 'Diff Current Conflict' } },
-          { 'n', '<leader>xdb', '<plug>(resolve-diff-both)', { desc = 'Diff Both Conflict' } },
-          { 'n', '<leader>xdv', '<plug>(resolve-diff-vs)', { desc = 'Diff Current V.S. Incoming Conflict' } },
-          { 'n', '<leader>xdV', '<plug>(resolve-diff-vs-reverse)', { desc = 'Diff Incoming V.S. Current Conflict' } },
+          { 'n', '<leader>xdi', resolve.show_diff_theirs, { desc = 'Diff Incoming Conflict' } },
+          { 'n', '<leader>xdc', resolve.show_diff_ours, { desc = 'Diff Current Conflict' } },
+          { 'n', '<leader>xdb', resolve.show_diff_both, { desc = 'Diff Both Conflict' } },
+          { 'n', '<leader>xdv', resolve.show_diff_ours_vs_theirs, { desc = 'Diff Current V.S. Incoming Conflict' } },
+          { 'n', '<leader>xdV', resolve.show_diff_theirs_vs_ours, { desc = 'Diff Incoming V.S. Current Conflict' } },
         })
+      end
+      if require('lightboat.util').plugin_available('which-key.nvim') then
+        local wk = require('which-key')
+        wk.add({ '<leader>x', desc = 'Conflict', buffer = args.bufnr })
+        wk.add({ '<leader>xd', desc = 'Conflict Diff', buffer = args.bufnr })
       end
       for _, m in ipairs(mapping) do
         m[4].buffer = args.bufnr
