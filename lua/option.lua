b82d09aeba48f59e284e8b02cc339f637384e8a1
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-21 03:00:34 +0800

    chore

--- lua/option.lua
+++ lua/option.lua
@@ -63,7 +63,7 @@ vim.g.blink_cmp_unique_priority = function(ctx)
 end
 vim.g.highlight_on_yank = true
 vim.g.highlight_on_yank_limit = 1024 -- 1 KB
-vim.g.highlight_on_yank_duration = 300
+vim.g.highlight_on_yank_duration = 300 -- Unit: ms
 vim.g.conform_on_save = true
 vim.g.conform_on_save_reguess_indent = true
 vim.g.conform_formatexpr_auto_set = true
@@ -72,7 +72,7 @@ vim.g.treesitter_highlight_auto_start = true
 vim.g.treesitter_foldexpr_auto_set = true
 vim.g.nohlsearch_auto_run = true
 vim.g.big_file_limit = 1024 * 1024 -- 1 MB
-vim.g.big_file_average_every_line = 200
+vim.g.big_file_average_every_line = 200 -- Unit: B
 local original = {}
 vim.g.big_file_callback = function(data)
   if not original[data.buffer] then original[data.buffer] = {} end
@@ -121,7 +121,7 @@ end
 -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
 vim.g.no_plugin_maps = true
 
--- We use another explore instead of netrw, so disable it
+-- We use another explorer instead of netrw, so disable it
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -145,12 +145,11 @@ vim.o.foldlevel = 99999
 vim.o.foldcolumn = '1'
 vim.o.fillchars = 'fold: ,foldopen:,foldclose:,foldsep: '
 vim.o.splitright = true
+vim.o.splitbelow = false
 vim.o.autowriteall = true
 
 vim.filetype.add({ pattern = { ['.*.bazelrc'] = 'bazelrc' } })
-
 vim.treesitter.language.register('objc', { 'objcpp' })
-vim.o.splitbelow = false
 vim.diagnostic.config({
   virtual_lines = { current_line = true },
 })
