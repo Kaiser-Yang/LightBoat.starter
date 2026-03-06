aa7b605e1743498f82b816724fcf7f886bc50ddf
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-03-06 21:25:16 +0800

    chore: do not show mode

--- lua/option.lua
+++ lua/option.lua
@@ -149,6 +149,7 @@ vim.o.fillchars = 'fold: ,foldopen:,foldclose:'
 vim.o.splitright = true
 vim.o.autowriteall = true
 vim.o.cmdwinheight = 10
+vim.o.showmode = false
 vim.opt.nrformats:append('octal')
 
 local function fold_clickable()
